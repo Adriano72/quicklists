7bce78b305bcc8941d290f023202afdc11ad0030
Adriano Massi <adrianomassi@gmail.com>
2016-07-17 04:30:32 +0200

    Pag 169

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,99 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, Alert} from 'ionic-angular';
+import {ChecklistPage} from '../checklist/checklist';
+import {ChecklistModel} from '../../providers/checklist-model/checklist-model';
+import {Data} from '../../providers/data/data';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navController: NavController) {
 
-  }
+    checklists: ChecklistModel[] = [];
+
+    constructor(public nav: NavController, public dataService: Data) {
+    }
+
+    addChecklist(): void {
+
+        let prompt = Alert.create({
+            title: 'New Checklist',
+            message: 'Enter the name of your new checklist below:',
+            inputs: [
+                {
+                    name: 'name'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel'
+                },
+                {
+                    text: 'Save',
+                    handler: data => {
+
+                        let newChecklist = new ChecklistModel(data.name, []);
+
+                        this.checklists.push(newChecklist);
+
+                        newChecklist.checklist.subscribe(update => {
+                            this.save();
+                        });
+
+                        this.save();
+
+                    }
+                }
+            ]
+        });
+
+        this.nav.present(prompt);
+
+    }
+
+    renameChecklist(checklist): void {
+        let prompt = Alert.create({
+            title: 'Rename Checklist',
+            message: 'Enter the new name of this checklist below:',
+            inputs: [
+                {
+                    name: 'name'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel'
+                },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        let index = this.checklists.indexOf(checklist);
+                        if (index > -1) {
+                            this.checklists[index].setTitle(data.name);
+                            this.save();
+                        }
+                    }
+                }
+            ]
+        });
+        this.nav.present(prompt);
+    }
+
+    viewChecklist(checklist): void {
+        this.nav.push(ChecklistPage, {
+            checklist: checklist
+        });
+    }
+
+    removeChecklist(checklist): void {
+        let index = this.checklists.indexOf(checklist);
+        if (index > -1) {
+            this.checklists.splice(index, 1);
+            this.save();
+        }
+    }
+
+    save(): void {
+    }
+
 }
